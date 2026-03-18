96c565a141858911474faa043ed5427eee00412f
Kostandinos Myrtollari <kostasmyrtollari@gmail.com>
2026-03-18 15:10:02 +0200

    changed parsing of sacct command (make use of -p)

--- job_monitoring_v2.go
+++ job_monitoring_v2.go
@@ -35,17 +35,17 @@ func GetPendingReasonV2(client *ssh.Client, id string) (string, error) {
 func PollingJobCompletionV2(client *ssh.Client, id string) (string, error) {
 	state := ""
 	for true {
-		cmd := fmt.Sprintf("sacct -j %s", id)
+		cmd := fmt.Sprintf("sacct -j %s -p", id)
 		output, err := ExecuteCmd(client, cmd)
 		if err != nil {
 			return state, fmt.Errorf("sacct execution for job %s failed: %v\nOutput: %s", id, err, output)
 		}
 
 		lines := strings.Split(strings.TrimSpace(output), "\n")
-		if len(lines) < 3 { // skip line 0 (headers) and line 1 (dashes)
+		if len(lines) < 2 {
 			return state, fmt.Errorf("not enough lines to parse")
 		}
-		fields := strings.Fields(lines[2]) //first actual data line
+		fields := strings.Split(lines[1], "|") // take the first data line and split by the pipe
 		if len(fields) >= 7 {
 			state = fields[5]
 		} else {
@@ -74,16 +74,16 @@ func ExtractJobDetailsV2(client *ssh.Client, id string) (string, error) {
 	partition := ""
 	state := ""
 	exitCode := ""
-	cmd := fmt.Sprintf("sacct -j %s", id)
+	cmd := fmt.Sprintf("sacct -j %s -p", id)
 	output, err := ExecuteCmd(client, cmd)
 	if err != nil {
 		return "", fmt.Errorf("sacct execution for job %s failed: %v\nOutput: %s", id, err, output)
 	}
 	lines := strings.Split(strings.TrimSpace(output), "\n")
-	if len(lines) < 3 { // skip line 0 (headers) and line 1 (dashes)
+	if len(lines) < 2 {
 		return "", fmt.Errorf("not enough lines to parse")
 	}
-	fields := strings.Fields(lines[2]) //first actual data line
+	fields := strings.Split(lines[1], "|") // take the first data line and split by the pipe
 	if len(fields) >= 7 {
 		jobName = fields[1]
 		partition = fields[2]
@@ -92,7 +92,7 @@ func ExtractJobDetailsV2(client *ssh.Client, id string) (string, error) {
 	} else {
 		return "", fmt.Errorf("not enough fields to parse")
 	}
-	return fmt.Sprintf("JobID:%s JobName:%s Partition:%s State:%s ExitCode:%s", id, jobName, partition, state, exitCode), nil
+	return fmt.Sprintf("JobID:%s|JobName:%s|Partition:%s|State:%s|ExitCode:%s", id, jobName, partition, state, exitCode), nil
 }
 
 func SaveJobStatusesV2(client *ssh.Client, ids []string, file string) error {
